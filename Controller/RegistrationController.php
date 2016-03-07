b89522afa67b6ed179d4641a9fc66a01efc7b009
Mishura Yuriy <Yuriy.Mishura@modera.net>
2016-03-07 18:20:51 +0200

    controller updated

--- Controller/RegistrationController.php
+++ Controller/RegistrationController.php
@@ -54,4 +54,24 @@ class RegistrationController extends BaseController
             'form' => $form->createView(),
         ));
     }
+
+    private function getErrorMessages(\Symfony\Component\Form\Form $form) {
+        $errors = array();
+
+        foreach ($form->getErrors() as $key => $error) {
+            if ($form->isRoot()) {
+                $errors['#'][] = $error->getMessage();
+            } else {
+                $errors[] = $error->getMessage();
+            }
+        }
+
+        foreach ($form->all() as $child) {
+            if (!$child->isValid()) {
+                $errors[$child->getName()] = $this->getErrorMessages($child);
+            }
+        }
+
+        return $errors;
+    }
 }
